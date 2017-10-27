c9694f458a873346eb0612c9c1e18d7d8d1ab6a3
houser <houser@xmind.net>
2017-10-27 18:14:59 +0800

    add framework

---
+++ Objective-Zip iOS/Objective_Zip_iOS.h
@@ -0,0 +1,18 @@
+//
+//  Objective_Zip_iOS.h
+//  Objective-Zip iOS
+//
+//  Created by Boring on 27/10/2017.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Objective_Zip_iOS.
+FOUNDATION_EXPORT double Objective_Zip_iOSVersionNumber;
+
+//! Project version string for Objective_Zip_iOS.
+FOUNDATION_EXPORT const unsigned char Objective_Zip_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Objective_Zip_iOS/PublicHeader.h>
+
+
